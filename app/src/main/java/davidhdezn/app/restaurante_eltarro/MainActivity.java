89ea6b3275b018054cc61790b04cb667f28b381b
David Hernandez <davidhdezn16@gmail.com>
2019-11-24 11:48:01 -0600

    Error de actualizaciòn en firebase

--- app/src/main/java/davidhdezn/app/restaurante_eltarro/MainActivity.java
+++ app/src/main/java/davidhdezn/app/restaurante_eltarro/MainActivity.java
@@ -2,10 +2,13 @@ package davidhdezn.app.restaurante_eltarro;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.graphics.Color;
+import android.graphics.drawable.ColorDrawable;
+import android.os.Build;
 import android.os.Bundle;
+import android.view.Window;
 
 public class MainActivity extends AppCompatActivity {
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
